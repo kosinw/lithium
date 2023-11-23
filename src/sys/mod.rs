1edd874a4d65ddb2472870a99c25d8db71277bfa
Kosi Nwabueze <kosinw41@gmail.com>
2023-11-23 00:07:01 -0500

    restarting everything, not using any external crates

--- src/sys/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod alloc;
-pub mod console;
-pub mod gdt;
-pub mod interrupts;
-pub mod memory;
-pub mod serial;
-pub mod time;
--- src/sys/time.rs
+++
@@ -1,3 +0,0 @@
-pub fn init() {
-    crate::info!("Initializing CMOS...");
-}
